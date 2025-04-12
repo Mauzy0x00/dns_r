1de2ea87cbb688eed702931e8736423d799209d8
Mauzy0x00 <mauzy.mail@proton.me>
2025-04-12 00:53:45 -0400

    implement question section; Refactor code to dns.rs; implement record type for common values; start the answer section

---
+++ src/dns.rs
@@ -0,0 +1,220 @@
+
+#[derive(Debug)]
+pub struct DnsHeader {
+                                        /*   https://www.rfc-editor.org/rfc/rfc1035#section-4.1.1   */
+                                        /*   https://en.wikipedia.org/wiki/Domain_Name_System#DNS_message_format   <-- The header format here is current   */ 
+
+    pub id: u16,                            // Transaction ID: 16 bits - A random ID assigned to query packets. Response packets must reply with the same ID.
+                                         // Flags: 16 bits
+    pub query_indicator: bool,              // QR: 1 bit - Indicates if the message is a query (0) or a reply (1).
+    pub opcode: u8,                         // OPCODE: 4 bits - The type can be QUERY (standard query, 0), IQUERY (inverse query, 1), or STATUS (server status request, 2).
+    pub authoritative_answer: bool,         // AA: 1 bit - Authoritative Answer, in a response, indicates if the DNS server is authoritative for the queried hostname.
+    pub truncation: bool,                   // TC: 1 bit - TrunCation, indicates that this message was truncated due to excessive length.
+    pub recursion_desired: bool,            // RD: 1 bit - Recursion Desired, indicates if the client means a recursive query.
+    pub recursion_available: bool,          // RA: 1 bit - Recursion Available, in a response, indicates if the replying DNS server supports recursion.
+    pub reserved: bool,                     // Z:  1 bit; (Z) == 0 - Zero, reserved for future use -> Now used for DNSSEC.
+    pub authentic_data: bool,               // AD: 1 bit - Authentic Data, in a response, indicates if the replying DNS server verified the data.
+    pub check_disabled: bool,               // CD: 1 bit - Checking Disabled, in a query, indicates that non-verified data is acceptable in a response.
+    pub response_code: u8,                  // RCODE: 4 bits - Response code, can be NOERROR (0), FORMERR (1, Format error), SERVFAIL (2), NXDOMAIN (3, Nonexistent domain), etc.
+
+    pub question_count: u16,                // Number of Questions: 16 bits
+    pub answer_record_count: u16,           // Number of Answers: 16 bits
+    pub authority_record_count: u16,        // Number of Authority RRs: 16 bits
+    pub additional_record_count: u16,       // Number of Additional RRs: 16 bits
+}
+
+impl DnsHeader {
+    const DNS_HEADER_LEN:usize = 12;
+
+    pub fn new() -> DnsHeader {
+        DnsHeader {
+            id: 0x00,                          
+    
+            query_indicator: false,              
+            opcode: 0,                         
+            authoritative_answer: false,               
+            truncation: false,                   
+            recursion_desired: false,            
+            recursion_available: false,          
+            reserved: false,                       
+            authentic_data: false,               
+            check_disabled: false,               
+            response_code: 0,                  
+    
+            question_count: 0,                
+            answer_record_count: 0,           
+            authority_record_count: 0,        
+            additional_record_count: 0, 
+        }
+    }
+
+    /// Convert each field of the DnsHeader struct to a Big Endian byte vector
+    pub fn serialize_to_bytes(&self) -> Vec<u8> {
+        
+        let mut buffer_vec = Vec::with_capacity(DnsHeader::DNS_HEADER_LEN);
+
+        buffer_vec.extend_from_slice(&self.id.to_be_bytes());     // u16 to big endian bytes
+        buffer_vec.push(
+            ((self.query_indicator as u8) << 7)                   // Convert to u8 then shift the bit 7 places to the left (most significant bit) - if true: 00000001 << 7  becomes  10000000 
+                | (self.opcode << 3)                              // shift the opcode(4 bits) left 3 bits and perform bitwise OR to the query_indicator bits 
+                                                                    //(ex. with opcode=1:  10000000 | (00000001 << 3) => 10000000 | 00001000 => resulting OR => 10001000) 
+                                                                    //                                         4 shifted opcode bits ^^^^                       ^   ^ significant bits remain after OR operation
+                | ((self.authoritative_answer as u8) << 2)
+                | ((self.truncation as u8) << 1)
+                | self.recursion_desired as u8,
+        );
+
+        // The same bit wise operations and big endian convertions occur for the rest of the DnsHeader fields... 
+        buffer_vec.push(
+            ((self.recursion_available as u8) << 7)         // 00000001 <<7 => 10000000
+                | ((self.reserved as u8) << 6)              // 00000001 <<6 => 01000000 | 10000000 => 11000000
+                | ((self.authentic_data as u8) << 5)        // 00000001 <<5 => 00100000 | 11000000 => 11100000
+                | ((self.check_disabled as u8) << 4)        // 00000001 <<4 => 00010000 | 11100000 => 11110000
+                | self.response_code,                       //                                            ^^^^ the 4 bit response_code already has it's signficant bits in the lower 4 bits, so just OR 
+        );
+
+        // Append remaining header fields
+        buffer_vec.extend_from_slice(&self.question_count.to_be_bytes());
+        buffer_vec.extend_from_slice(&self.answer_record_count.to_be_bytes());
+        buffer_vec.extend_from_slice(&self.authority_record_count.to_be_bytes());
+        buffer_vec.extend_from_slice(&self.additional_record_count.to_be_bytes());
+
+
+        buffer_vec
+    }
+}
+
+
+/// The question section has a simpler format than the resource record format used in the other sections. Each question record (there is usually just one in the section)
+pub struct QuestionSection {
+    // The domain name is broken into discrete labels which are concatenated; each label is prefixed by the length of that label
+    pub name: String,          // The domain name, encoded as a sequence of labels
+    pub record_type: u16,      // 2 byte integer that defines the record type : Type of RR (A, AAAA, MX, TXT, etc.)
+    pub record_class: u16,     // 2 byte integer that defines the record class
+}   
+
+impl QuestionSection {
+    pub fn new() -> QuestionSection {
+        QuestionSection { name: String::new(), record_type: 1, record_class: 1}
+    }
+
+    pub fn to_label_sequence(&self) -> String {
+
+        // Separate domain name by '.' ; Get length of the first label; place length in hex to the front; get length of second label (TDL); replace with length in hex; append null byte
+        // <length><content>
+        let domain_name = &self.name;
+        let split_domain_name: Vec<&str> = domain_name.split('.').collect();
+
+        let mut label_sequence = String::new();
+
+        for content_label in split_domain_name {
+            // Get the length of the current label and convert it to hex (format: \x0b)
+            let this_str_len = content_label.len();
+            let length_label = format!("\\x{:02x}", this_str_len);  // Format the string as a 2 byte hex value
+
+            // Append the length label and content label
+            label_sequence += &length_label;
+            label_sequence += content_label;
+        }
+
+        label_sequence += "\\x00";  // Append a null byte to the label sequence
+        
+        label_sequence
+    }
+
+    /// Convert each field of the QuestionSection struct to a Big Endian byte vector
+    pub fn serialize_to_bytes(&self) -> Vec<u8> {
+
+        let capacity = self.name.len() + 32;    // Capacity is the length of the name + the 4 bytes of the record_type and record_class field 
+
+        let mut buffer_vec: Vec<u8> = Vec::with_capacity(capacity);
+
+        // Clone the name (which at this point should be a label) and convert it to bytes
+        let name = self.name.clone();
+        let mut name_bytes = name.into_bytes();
+
+        buffer_vec.append(&mut name_bytes);
+    
+        // Append remaining header fields
+        buffer_vec.extend_from_slice(&self.record_type.to_be_bytes());
+        buffer_vec.extend_from_slice(&self.record_class.to_be_bytes());
+
+        buffer_vec
+    }
+}
+
+struct ResourceRecord {
+                            /*   https://en.wikipedia.org/wiki/Domain_Name_System#Resource_records   */
+    name: String,               // [Variable size] Name of the node to which this record pertains
+    record_type: RecordType,    // 2 byte 	Type of resource record in numeric form (e.g., 15 for MX RRs)
+    class: u16,                 // 2 byte   class code
+    ttl: u32,                   // 4 byte   Count of seconds that the RR stays valid (The maximum is 231−1, which is about 68 years)
+    record_data_length: u16,    // 2 byte   Length of RDATA field (specified in octets)
+    record_data: Vec<u8>,        // [Variable size] Additonal resource record specific data
+}
+
+impl ResourceRecord {
+
+    pub fn new() -> ResourceRecord {
+        ResourceRecord { 
+            name: String::new(), 
+            record_type: RecordType::A, 
+            class: 0, 
+            ttl: 0, 
+            record_data_length: 0, 
+            record_data: Vec::new()
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum RecordType {
+    A,               //1 a host address
+    NS,              //2 an authoritative name server
+    MD,              //3 a mail destination (Obsolete - use MX)
+    MF,              // 4 a mail forwarder (Obsolete - use MX)
+    CNAME,           // 5 the canonical name for an alias
+    SOA,             // 6 marks the start of a zone of authority
+    MB,              // 7 a mailbox domain name (EXPERIMENTAL)
+    MG,              // 8 a mail group member (EXPERIMENTAL)
+    MR,              // 9 a mail rename domain name (EXPERIMENTAL)
+    NULL,            // 10 a null RR (EXPERIMENTAL)
+    WKS,             // 11 a well known service description
+    PTR,             // 12 a domain name pointer
+    HINFO,           // 13 host information
+    MINFO,           // 14 mailbox or mail list information
+    MX,              // 15 mail exchange
+    TXT,             // 16 text strings
+}
+impl RecordType {
+    pub fn to_u16(&self) -> u16 {
+        match self {
+            RecordType::A => 1,
+            RecordType::NS => 2,
+            RecordType::MD => 3,
+            RecordType::MF => 4,
+            RecordType::CNAME => 5,
+            RecordType::SOA => 6,
+            RecordType::MB => 7,
+            RecordType::MG => 8,
+            RecordType::MR => 9,
+            RecordType::NULL => 10,
+            RecordType::WKS => 11,
+            RecordType::PTR => 12,
+            RecordType::HINFO => 13,
+            RecordType::MINFO => 14,
+            RecordType::MX => 15,
+            RecordType::TXT => 16,
+        }
+    }
+}
+
+
+pub struct AnswerSection {
+    name: String,
+    record_type: u16,
+    class: u16,
+    ttl: u32,
+    length: u16,
+    data: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -6,171 +6,9 @@
 
 use std::net::UdpSocket;
 
-#[derive(Debug)]
-struct DnsHeader {
-                                        /*   https://www.rfc-editor.org/rfc/rfc1035#section-4.1.1   */
-                                        /*   https://en.wikipedia.org/wiki/Domain_Name_System#DNS_message_format   <-- The header format here is current   */ 
+mod dns;
+use dns::*;
 
-    id: u16,                            // Transaction ID: 16 bits - A random ID assigned to query packets. Response packets must reply with the same ID.
-                                    // Flags: 16 bits
-    query_indicator: bool,              // QR: 1 bit - Indicates if the message is a query (0) or a reply (1).
-    opcode: u8,                         // OPCODE: 4 bits - The type can be QUERY (standard query, 0), IQUERY (inverse query, 1), or STATUS (server status request, 2).
-    authoritative_answer: bool,         // AA: 1 bit - Authoritative Answer, in a response, indicates if the DNS server is authoritative for the queried hostname.
-    truncation: bool,                   // TC: 1 bit - TrunCation, indicates that this message was truncated due to excessive length.
-    recursion_desired: bool,            // RD: 1 bit - Recursion Desired, indicates if the client means a recursive query.
-    recursion_available: bool,          // RA: 1 bit - Recursion Available, in a response, indicates if the replying DNS server supports recursion.
-    reserved: bool,                     // Z:  1 bit; (Z) == 0 - Zero, reserved for future use -> Now used for DNSSEC.
-    authentic_data: bool,               // AD: 1 bit - Authentic Data, in a response, indicates if the replying DNS server verified the data.
-    check_disabled: bool,               // CD: 1 bit - Checking Disabled, in a query, indicates that non-verified data is acceptable in a response.
-    response_code: u8,                  // RCODE: 4 bits - Response code, can be NOERROR (0), FORMERR (1, Format error), SERVFAIL (2), NXDOMAIN (3, Nonexistent domain), etc.
-
-    question_count: u16,                // Number of Questions: 16 bits
-    answer_record_count: u16,           // Number of Answers: 16 bits
-    authority_record_count: u16,        // Number of Authority RRs: 16 bits
-    additional_record_count: u16,       // Number of Additional RRs: 16 bits
-}
-
-impl DnsHeader {
-    const DNS_HEADER_LEN:usize = 12;
-
-    pub fn new() -> DnsHeader {
-        DnsHeader {
-            id: 0x00,                          
-    
-            query_indicator: false,              
-            opcode: 0,                         
-            authoritative_answer: false,               
-            truncation: false,                   
-            recursion_desired: false,            
-            recursion_available: false,          
-            reserved: false,                       
-            authentic_data: false,               
-            check_disabled: false,               
-            response_code: 0,                  
-    
-            question_count: 0,                
-            answer_record_count: 0,           
-            authority_record_count: 0,        
-            additional_record_count: 0, 
-        }
-    }
-
-    /// Convert each field of the DnsHeader struct to a Big Endian byte vector
-    pub fn serialize_to_bytes(&self) -> Vec<u8> {
-        
-        let mut buffer_vec = Vec::with_capacity(DnsHeader::DNS_HEADER_LEN);
-
-        buffer_vec.extend_from_slice(&self.id.to_be_bytes());     // u16 to big endian bytes
-        buffer_vec.push(
-            ((self.query_indicator as u8) << 7)                   // Convert to u8 then shift the bit 7 places to the left (most significant bit) - if true: 00000001 << 7  becomes  10000000 
-                | (self.opcode << 3)                              // shift the opcode(4 bits) left 3 bits and perform bitwise OR to the query_indicator bits 
-                                                                    //(ex. with opcode=1:  10000000 | (00000001 << 3) => 10000000 | 00001000 => resulting OR => 10001000) 
-                                                                    //                                         4 shifted opcode bits ^^^^                       ^   ^ significant bits remain after OR operation
-                | ((self.authoritative_answer as u8) << 2)
-                | ((self.truncation as u8) << 1)
-                | self.recursion_desired as u8,
-        );
-
-        // The same bit wise operations and big endian convertions occur for the rest of the DnsHeader fields... 
-        buffer_vec.push(
-            ((self.recursion_available as u8) << 7)         // 00000001 <<7 => 10000000
-                | ((self.reserved as u8) << 6)              // 00000001 <<6 => 01000000 | 10000000 => 11000000
-                | ((self.authentic_data as u8) << 5)        // 00000001 <<5 => 00100000 | 11000000 => 11100000
-                | ((self.check_disabled as u8) << 4)        // 00000001 <<4 => 00010000 | 11100000 => 11110000
-                | self.response_code,                       //                                            ^^^^ the 4 bit response_code already has it's signficant bits in the lower 4 bits, so just OR 
-        );
-
-        // Append remaining header fields
-        buffer_vec.extend_from_slice(&self.question_count.to_be_bytes());
-        buffer_vec.extend_from_slice(&self.answer_record_count.to_be_bytes());
-        buffer_vec.extend_from_slice(&self.authority_record_count.to_be_bytes());
-        buffer_vec.extend_from_slice(&self.additional_record_count.to_be_bytes());
-
-
-        buffer_vec
-    }
-}
-
-/// The question section has a simpler format than the resource record format used in the other sections. Each question record (there is usually just one in the section)
-struct QuestionSection {
-    // The domain name is broken into discrete labels which are concatenated; each label is prefixed by the length of that label
-    name: String,          // The domain name, encoded as a sequence of labels
-    record_type: u16,      // 2 byte integer that defines the record type : Type of RR (A, AAAA, MX, TXT, etc.)
-}   
-
-impl QuestionSection {
-    pub fn new() -> QuestionSection {
-        QuestionSection { name: String::new(), record_type: 0}
-    }
-
-    pub fn to_label_sequence(mut self) -> String {
-
-        // Separate domain name by '.' ; Get length of the first label; place length in hex to the front; get length of second label (TDL); replace with length in hex; append null byte
-        // <length><content>
-        let domain_name = &self.name;
-        let split_domain_name: Vec<&str> = domain_name.split('.').collect();
-
-        let mut label_sequence = String::new();
-
-        for content_label in split_domain_name {
-            // Get the length of the current label and convert it to hex (format: \x0b)
-            let this_str_len = content_label.len();
-            let length_label = format!("\\x{:x}", this_str_len);
-
-            // Append the length label and content label
-            label_sequence += &length_label;
-            label_sequence += content_label;
-        }
-
-        label_sequence += "\\x00";  // Append a null byte to the label sequence
-        self.name = label_sequence;
-
-        self.name
-    }
-}
-
-// struct ResourceRecord {
-//                             /*   https://en.wikipedia.org/wiki/Domain_Name_System#Resource_records   */
-//     name: String,               // [Variable size] Name of the node to which this record pertains
-//     record_type: RecordType,    // 2 byte 	Type of resource record in numeric form (e.g., 15 for MX RRs)
-//     class: u16,                 // 2 byte   class code
-//     ttl: u32,                   // 4 byte   Count of seconds that the RR stays valid (The maximum is 231−1, which is about 68 years)
-//     record_data_length: u16,    // 2 byte   Length of RDATA field (specified in octets)
-//     record_data: String,        // [Variable size] Additonal resource record specific data
-// }
-
-// impl ResourceRecord {
-
-//     pub fn new() -> ResourceRecord {
-//         ResourceRecord { 
-//             name: String::new(), 
-//             record_type: RecordType::A, 
-//             class: 0, 
-//             ttl: 0, 
-//             record_data_length: 0, 
-//             record_data: String::new() 
-//         }
-//     }
-// }
-
-// enum RecordType {
-    // A,               //1 a host address
-    // NS,              //2 an authoritative name server
-    // MD,              //3 a mail destination (Obsolete - use MX)
-    // MF,              // 4 a mail forwarder (Obsolete - use MX)
-    // CNAME,           // 5 the canonical name for an alias
-    // SOA,             // 6 marks the start of a zone of authority
-    // MB,              // 7 a mailbox domain name (EXPERIMENTAL)
-    // MG,              // 8 a mail group member (EXPERIMENTAL)
-    // MR,              // 9 a mail rename domain name (EXPERIMENTAL)
-    // NULL,            // 10 a null RR (EXPERIMENTAL)
-    // WKS,             // 11 a well known service description
-    // PTR,             // 12 a domain name pointer
-    // HINFO,           // 13 host information
-    // MINFO,           // 14 mailbox or mail list information
-    // MX,              // 15 mail exchange
-    // TXT,             // 16 text strings
-// }
 
 
 fn main() -> std::io::Result<()> {
@@ -190,12 +28,22 @@ fn main() -> std::io::Result<()> {
     default_response.query_indicator = true;
     default_response.question_count = 1;
 
-
+    // Setup question section
+    let domain_name = "google.com";
     let mut question = QuestionSection::new();
+
+
+    // Add the domain name to the name field and convert it to a label sequence
+    question.name = domain_name.to_string();
+    question.name = question.to_label_sequence();
     question.record_type = 1;
+    question.record_class = 1;
+
+    println!("Question name label: {}", question.name);
 
     // Serialize the data and send to the client
-    let serialized_response = default_response.serialize_to_bytes();
+    let mut serialized_response = default_response.serialize_to_bytes();
+    serialized_response.append(&mut question.serialize_to_bytes());     // Append the QuestionSection to the response
 
     display_sent_values(&serialized_response);
 
